9a31b35446560f98d382f93c3ffaa2e01e97874c
Adrien Pyke <adpyke@gmail.com>
2016-06-27 18:57:42 -0400

    conversion to es6

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 // Fork of Audioshield-Tubifier by https://www.reddit.com/user/-olli-
 
 // Load the credentails
-var API_KEY = require('./apikey.json');
+const API_KEY = require('./apikey.json');
 
 // Load a template for an empty response from Soundcloud API. This will be filled with values from the Play Music API.
-var soundcloudTemplate = require('./soundcloud_template.json');
+const soundcloudTemplate = require('./soundcloud_template.json');
 
 // Required libraries
-var fs = require('fs');
-var PlayMusic = require('playmusic');
-var ffmpeg = require('fluent-ffmpeg');
-var https = require('https');
-var request = require("request");
-var express = require('express');
+const fs = require('fs');
+const PlayMusic = require('playmusic');
+const ffmpeg = require('fluent-ffmpeg');
+const https = require('https');
+const request = require("request");
+const express = require('express');
 
 var app = express();
 var pm = new PlayMusic();
@@ -26,7 +26,7 @@ var options = {
 };
 
 var parseTracks = function(tracks) {
-	var output = tracks.map(function(track) {
+	var output = tracks.map((track) => {
 		var soundcloudResponse = JSON.parse(JSON.stringify(soundcloudTemplate));
 		// Fill the template with values from the Play Music API response
 		// Note specially the stream_url -field, which is set to correspond to our /stream HTTPS endpoint
@@ -34,8 +34,8 @@ var parseTracks = function(tracks) {
 		soundcloudResponse.id = track.track.nid;
 		soundcloudResponse.artwork_url = track.track.albumArtRef[0].url;
 		soundcloudResponse.user.username = track.track.artist;
-		soundcloudResponse.stream_url = 'https://api.soundcloud.com/stream?id=' + soundcloudResponse.id;
-		soundcloudResponse.uri = 'https://api.soundcloud.com/stream/' + soundcloudResponse.id;
+		soundcloudResponse.stream_url = `https://api.soundcloud.com/stream?id=${soundcloudResponse.id}`;
+		soundcloudResponse.uri = `https://api.soundcloud.com/stream/${soundcloudResponse.id}`;
 		soundcloudResponse.permalink = soundcloudResponse.id;
 		soundcloudResponse.permalink_url = soundcloudResponse.uri;
 
@@ -48,7 +48,7 @@ var parseTracks = function(tracks) {
 
 // Here we register a HTTPS endpoint that streams a given Play Music ID as an mp3-file
 // Expected URL: https://api.soundcloud.com/stream?id=<ID>
-app.get('/stream', function (req, res) {
+app.get('/stream', (req, res) => {
 	// Are we missing the id -parameter or is it empty?
 	if ((typeof req.query.id === 'undefined') || (req.query.id.length === 0)) {
 		res.writeHead(500);
@@ -58,13 +58,13 @@ app.get('/stream', function (req, res) {
 		// Audioshield appends any stream url it gets with another query variable, so we have to split it, and only use the first part
 		var id = req.query.id.split("?")[0];
 	
-		console.log("Got a stream request for Play Music id: " + id);
+		console.log(`Got a stream request for Play Music id: ${id}`);
 
-		pm.init({androidId: API_KEY.androidId, masterToken: API_KEY.masterToken}, function(err) {
+		pm.init({androidId: API_KEY.androidId, masterToken: API_KEY.masterToken}, (err) => {
 			if (err) throw err;
 
 			// load audio
-			pm.getStreamUrl(id, function(err, url) {
+			pm.getStreamUrl(id, (err, url) => {
 				if (err) throw err;
 
 				// Set the HTTP-headers to audio/mpeg
@@ -75,7 +75,7 @@ app.get('/stream', function (req, res) {
 				proc = new ffmpeg({source: url});
 				
 				// Error handling
-				proc.on('error', function(err, stdout, stderr) { 
+				proc.on('error', (err, stdout, stderr) => { 
 					// "Output stream closed" error message is ignored, it is caused by browsers doing a double HTTP-request
 					if (err.message != "Output stream closed") throw err;
 				});
@@ -92,25 +92,25 @@ app.get('/stream', function (req, res) {
 // Here we register a HTTPS endpoint that handles searching Play Music for tracks
 // This endpoint format is set by Audioshield, so we have to follow it
 // Expected URL: https://api.soundcloud.com/tracks?q=<SearchTerms>
-app.get('/tracks', function (req, res) {
+app.get('/tracks', (req, res) => {
 	// Are we missing the q -parameter or is it empty?	
 	if ((typeof req.query.q === 'undefined') || (req.query.q.length === 0)) {
 		res.writeHead(500);
 		res.end("parameter 'q' missing or empty");
 	} else {
 		// q -parameter found
-		console.log("Got a search request: " + req.query.q);
+		console.log(`Got a search request: ${req.query.q}`);
 
-		pm.init({androidId: API_KEY.androidId, masterToken: API_KEY.masterToken}, function(err) {
+		pm.init({androidId: API_KEY.androidId, masterToken: API_KEY.masterToken}, (err) => {
 			if (err) throw err;
 
-			pm.search(req.query.q, 20, function(err, data) {
+			pm.search(req.query.q, 20, (err, data) => {
 				if (err) console.error(err);
 				else {
 					// only return songs
-					var tracks = data.entries.filter(function(entry) {
+					var tracks = data.entries.filter((entry) => {
 						return entry.type === '1';
-					}).sort(function(a, b) {
+					}).sort((a, b) => {
 						return a.score < b.score;
 					});
 
@@ -133,9 +133,9 @@ if (API_KEY.androidId !== "-1" && API_KEY.masterToken !== '-1') {
 	var httpsServer = https.createServer(options, app);
 	httpsServer.listen(443);
 
-	console.log("Server running");
-	console.log("CTRL+C to shutdown");
+	console.log('Server running');
+	console.log('CTRL+C to shutdown');
 } else {
 	// Credentials key has not been set
-	console.log("API keys have not been set. Login using login.js.");
+	console.log('API keys have not been set. Login using login.js.');
 }
--- login.js
+++ login.js
@@ -1,14 +1,14 @@
-var PM_CRED = require('./pmcred.json');
-var PlayMusic = require('playmusic');
+const PM_CRED = require('./pmcred.json');
+const PlayMusic = require('playmusic');
 var pm = new PlayMusic();
 
 if (PM_CRED.email !== -1) {
-	pm.login({email: PM_CRED.email, password: PM_CRED.password}, function(err, resp) {
+	pm.login({email: PM_CRED.email, password: PM_CRED.password}, (err, resp) => {
 		if (err) console.error(err);
 		else {
-			console.log("Logged in to Play Music, enter the following data into apikey.json. You can now clear pmcred.json.");
-			console.log("androidId: " + resp.androidId);
-			console.log("masterToken: " + resp.masterToken);
+			console.log('Logged in to Play Music, enter the following data into apikey.json. You can now clear pmcred.json.');
+			console.log(`androidId: ${resp.androidId}`);
+			console.log(`masterToken: ${resp.masterToken}`);
 		}
 	});
 } else {
