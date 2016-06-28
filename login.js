07530630e8940aeef750eed391790625e64a9ed4
Adrien Pyke <adpyke@gmail.com>
2016-06-27 22:13:27 -0400

    reformat

--- login.js
+++ login.js
@@ -8,18 +8,18 @@ var pm = new PlayMusic();
 read({prompt: 'Email: '}, (err, email) => {
 	read({prompt: 'Password (generate an app password if using 2 factor authentication): ', silent: true}, (err, password) => {
 		pm.login({email: email, password: password}, (err, resp) => {
-		if (err) 
-			console.log('Email or Password incorrect.');
-		else {
-			apikey.androidId = resp.androidId;
-			apikey.masterToken = resp.masterToken;
-			fs.writeFile(API_FILE, JSON.stringify(apikey, null, 4), (err) => {
-				if (err) 
-					console.error(err);
-				else 
-					console.log('Successfully logged into Google Play Music.');
-			});
-		}
-	});
+			if (err) 
+				console.log('Email or Password incorrect.');
+			else {
+				apikey.androidId = resp.androidId;
+				apikey.masterToken = resp.masterToken;
+				fs.writeFile(API_FILE, JSON.stringify(apikey, null, 4), (err) => {
+					if (err) 
+						console.error(err);
+					else 
+						console.log('Successfully logged into Google Play Music.');
+				});
+			}
+		});
 	});
 });
