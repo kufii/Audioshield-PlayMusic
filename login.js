c6a373848c832e562281d61bf32f11757e74b6d8
Adrien Pyke <adpyke@gmail.com>
2016-06-28 00:21:54 -0400

    updated my prompts to use gen-run, much nicer

--- login.js
+++ login.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
 const read = require('read');
+const run = require('gen-run');
 const PlayMusic = require('playmusic');
 const API_FILE = './apikey.json';
 var apikey = require(API_FILE);
 var pm = new PlayMusic();
 
-read({prompt: 'Email: '}, (err, email) => {
-	read({prompt: 'Password (generate an app password if using 2 factor authentication): ', silent: true}, (err, password) => {
-		pm.login({email: email, password: password}, (err, resp) => {
-			if (err) 
-				console.log('Email or Password incorrect.');
-			else {
-				apikey.androidId = resp.androidId;
-				apikey.masterToken = resp.masterToken;
-				fs.writeFile(API_FILE, JSON.stringify(apikey, null, 4), (err) => {
-					if (err) 
-						console.error(err);
-					else 
-						console.log('Successfully logged into Google Play Music.');
-				});
-			}
-		});
+
+run(function* (gen) {
+	var email = yield read({prompt: 'Email: '}, gen());
+	var password = yield read({prompt: 'Password (generate an app password if using 2 factor authentication): ', silent: true}, gen());
+
+	pm.login({email: email, password: password}, (err, resp) => {
+		if (err) 
+			console.log('Email or Password incorrect.');
+		else {
+			apikey.androidId = resp.androidId;
+			apikey.masterToken = resp.masterToken;
+			fs.writeFile(API_FILE, JSON.stringify(apikey, null, 4), (err) => {
+				if (err) 
+					console.error(err);
+				else 
+					console.log('Successfully logged into Google Play Music.');
+			});
+		}
 	});
 });
