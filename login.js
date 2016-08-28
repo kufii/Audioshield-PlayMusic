4d3d08690cd9f79a5efc3ca10f582a04e0e17089
Adrien Pyke <adpyke@gmail.com>
2016-08-28 17:49:29 -0400

    warnings as errors, removed trailing whitespace

--- login.js
+++ login.js
@@ -11,16 +11,16 @@ run(function* (gen) {
 	var password = yield read({prompt: 'Password (generate an app password if using 2 factor authentication): ', silent: true}, gen());
 
 	pm.login({email: email, password: password}, (err, resp) => {
-		if (err) 
+		if (err)
 			console.log('Email or Password incorrect.');
 		else {
 			var apikey = {};
 			apikey.androidId = resp.androidId;
 			apikey.masterToken = resp.masterToken;
 			fs.writeFile(API_FILE, JSON.stringify(apikey, null, 4), (err) => {
-				if (err) 
+				if (err)
 					console.error(err);
-				else 
+				else
 					console.log('Successfully logged into Google Play Music.');
 			});
 		}
